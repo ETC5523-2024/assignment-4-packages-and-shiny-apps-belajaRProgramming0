770642a734d84a0c56e3401d2f27205f278ced23
Rafli Ramdani <rram0049@student.monash.edu>
2024-10-31 01:46:47 +1100

    adding cleaned data rda file and do devtools document

--- data-raw/cleaning-data.R
+++ data-raw/cleaning-data.R
@@ -3,6 +3,7 @@
 # Load required libraries
 library(dplyr)
 library(readr)
+library(tidyverse)
 
 # Load the data
 noc_region <- read_csv("data-raw/noc_region.csv")
@@ -11,7 +12,7 @@ noc_region <- read_csv("data-raw/noc_region.csv")
 noc_region <- noc_region[, -3]
 
 # Load the Olympics data
-olympics <- read_csv("data-raw/olympics.csv")
+olympics <- read_csv("data-raw/olympic.csv")
 
 # Perform inner join on the noc_region and olympics data
 cleaned_data <- inner_join(olympics, noc_region, by = c("Team" = "reg"))
